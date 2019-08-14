77d0a82067bdd1790cb37167953549bd8b6bcd94
sdetuser <Filosova23@gmail.com>
2019-08-13 21:39:54 -0400

    Added Contacts tests

--- src/test/java/tests/ContactTest.java
+++ src/test/java/tests/ContactTest.java
@@ -6,6 +6,7 @@ public class ContactTest {
 
     @Test
     public void test1(){
-
+        System.out.println("driver open vytrack:)");
+        System.out.println("go to contacts page!!!!");
     }
 }
