94e6fbf51bb6aaebcee21e10d4af2766aa047cfe
sdetuser <Filosova23@gmail.com>
2019-08-13 21:32:58 -0400

    Added Contacts tests

---
+++ src/test/java/tests/ContactTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+    @Test
+    public void test1(){
+
+    }
+}
--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -4,5 +4,6 @@ public class FirstTest {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Test");
     }
 }
